c359ae8b45a39ae55e49bfb28916e678020e2c14
Max Weller <git@max-weller.de>
2017-10-05 00:40:03 +0200

    RemoteUdpLogging: add timestamp, display boot reason

--- framework/util/RemoteUdpLogging.h
+++ framework/util/RemoteUdpLogging.h
@@ -5,12 +5,15 @@ static UdpConnection remote_udp_log_socket;
 static IPAddress remote_udp_log_ip;
 static uint16_t remote_udp_log_port;
 
-char remote_udp_log_buf[512];
+char remote_udp_log_buf[320];
 char* remote_udp_log_ptr = NULL;
 void remote_udp_log_putc(uart_t * dummy, char c) {
     if (remote_udp_log_ptr == NULL) remote_udp_log_ptr = remote_udp_log_buf;
     *(remote_udp_log_ptr++) = c;
-    if (remote_udp_log_ptr - remote_udp_log_buf == 511) {
+    if (c == '\n') {
+        remote_udp_log_ptr += sprintf(remote_udp_log_ptr, "%012d\t", RTC.getRtcSeconds());
+    }
+    if (remote_udp_log_ptr - remote_udp_log_buf >= 300) {
         *remote_udp_log_ptr = '\0';
         remote_udp_log_socket.sendStringTo(remote_udp_log_ip,remote_udp_log_port,remote_udp_log_buf);
         remote_udp_log_ptr = remote_udp_log_buf;
@@ -20,11 +23,15 @@ void remote_udp_log_putc(uart_t * dummy, char c) {
 //set m_printf callback
 extern void setMPrintfPrinterCbc(void (*callback)(uart_t *, char), uart_t *uart);
 
+
 void remote_udp_log_enable(IPAddress targetIp, uint16_t targetPort) {
     debug_i("Enabling remote UDP logging, destination=%s:%d", targetIp.toString().c_str(), targetPort);
     remote_udp_log_ip = targetIp;
     remote_udp_log_port = targetPort;
     setMPrintfPrinterCbc(remote_udp_log_putc, NULL);
-    debug_d("remote_udp_log enabled"); // this goes out via remote UDP logging
+    debugf("------------------------------"); // this goes out via remote UDP logging
+    debugf("remote_udp_log enabled"); // this goes out via remote UDP logging
+	rst_info* info = system_get_rst_info();
+    debugf("boot reason %d", info->reason); // this goes out via remote UDP logging
 }
 #endif
